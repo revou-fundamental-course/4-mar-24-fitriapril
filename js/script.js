ac9291a11ce223fdcd53e039894172614a1145cf
unknown <aprianti0104@gmail.com>
2024-03-12 14:09:19 +0700

    revisi

--- js/script.js
+++ js/script.js
@@ -1,79 +1,37 @@
-(()=>{
-    "use strict";
-    var e, v = {}, _ = {};
-    function a(e) {
-        var n = _[e];
-        if (void 0 !== n)
-            return n.exports;
-        var r = _[e] = {
-            exports: {}
-        };
-        return v[e](r, r.exports, a),
-        r.exports
+let button = document.getElementById('btn');
+button .addEventListener ('click',() => {
+  const height = parseInt(document.getElementById ('height').value);
+  const weight = parseInt(document.getElementById ('weight').value);
+  const result = parseInt(document.getElementById ('output'));
+  let height_status=false, weight_status=false;
+
+
+  if(height === ''||isNaN(height) ||(height <= 0)){
+    document.getElementById('height_error').innerHTML='Tinggi badan Error,ulangi dengan benar';
+  }else{
+    document.getElementById('height_error').innerHTML='';
+    height_status=true;
+  }
+  if(weight === ''||isNaN(weight) ||(weight <= 0)){
+    document.getElementById('weight_error').innerHTML='Berat badan Error,ulangi dengan benar';
+  }else{
+    document.getElementById('weight_error').innerHTML='';
+    weight_status=true;
+  }
+  if(height_status && weight_status){
+    const bmi = (weight / ((height*height)/10000)).toFixed(2);
+  
+    if (bmi < 18.5) {
+      result.innerHTML = "Underweight" + bmi;
+    } else if (bmi >=18.5 && bmi < 25) {
+      result.innerHTML = "Normal weight" + bmi;
+    } else if (bmi < 30) {
+      result.innerHTML = "Overweight"+ bmi;
+    } else {
+      alert('Hasil Error,Ulangi!');
+      result.innerHTML ='';
     }
-    a.m = v,
-    e = [],
-    a.O = (n,r,t,o)=>{
-        if (!r) {
-            var c = 1 / 0;
-            for (l = 0; l < e.length; l++) {
-                for (var [r,t,o] = e[l], i = !0, u = 0; u < r.length; u++)
-                    (!1 & o || c >= o) && Object.keys(a.O).every(s=>a.O[s](r[u])) ? r.splice(u--, 1) : (i = !1,
-                    o < c && (c = o));
-                if (i) {
-                    e.splice(l--, 1);
-                    var f = t();
-                    void 0 !== f && (n = f)
-                }
-            }
-            return n
-        }
-        o = o || 0;
-        for (var l = e.length; l > 0 && e[l - 1][2] > o; l--)
-            e[l] = e[l - 1];
-        e[l] = [r, t, o]
-    }
-    ,
-    a.d = (e,n)=>{
-        for (var r in n)
-            a.o(n, r) && !a.o(e, r) && Object.defineProperty(e, r, {
-                enumerable: !0,
-                get: n[r]
-            })
-    }
-    ,
-    a.o = (e,n)=>Object.prototype.hasOwnProperty.call(e, n),
-    a.r = e=>{
-        typeof Symbol < "u" && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
-            value: "Module"
-        }),
-        Object.defineProperty(e, "__esModule", {
-            value: !0
-        })
-    }
-    ,
-    (()=>{
-        var e = {
-            666: 0
-        };
-        a.O.j = t=>0 === e[t];
-        var n = (t,o)=>{
-            var u, f, [l,c,i] = o, b = 0;
-            if (l.some(p=>0 !== e[p])) {
-                for (u in c)
-                    a.o(c, u) && (a.m[u] = c[u]);
-                if (i)
-                    var d = i(a)
-            }
-            for (t && t(o); b < l.length; b++)
-                a.o(e, f = l[b]) && e[f] && e[f][0](),
-                e[f] = 0;
-            return a.O(d)
-        }
-          , r = self.webpackChunkbmi_calculator_app = self.webpackChunkbmi_calculator_app || [];
-        r.forEach(n.bind(null, 0)),
-        r.push = n.bind(null, r.push.bind(r))
-    }
-    )()
-}
-)();
+  
+  }
+});
+
