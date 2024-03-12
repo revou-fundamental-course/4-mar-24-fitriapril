2d53c4096a019cd3e6af1d7816088962093b95bd
Fitri Aprianti Fauziah <155968322+fitriapril@users.noreply.github.com>
2024-03-12 14:20:37 +0700

    revisi

---
+++ revou_fitri/js/script.js
@@ -0,0 +1,37 @@
+let button = document.getElementById('btn');
+button .addEventListener ('click',() => {
+  const height = parseInt(document.getElementById ('height').value);
+  const weight = parseInt(document.getElementById ('weight').value);
+  const result = parseInt(document.getElementById ('output'));
+  let height_status=false, weight_status=false;
+
+
+  if(height === ''||isNaN(height) ||(height <= 0)){
+    document.getElementById('height_error').innerHTML='Tinggi badan Error,ulangi dengan benar';
+  }else{
+    document.getElementById('height_error').innerHTML='';
+    height_status=true;
+  }
+  if(weight === ''||isNaN(weight) ||(weight <= 0)){
+    document.getElementById('weight_error').innerHTML='Berat badan Error,ulangi dengan benar';
+  }else{
+    document.getElementById('weight_error').innerHTML='';
+    weight_status=true;
+  }
+  if(height_status && weight_status){
+    const bmi = (weight / ((height*height)/10000)).toFixed(2);
+  
+    if (bmi < 18.5) {
+      result.innerHTML = "Underweight" + bmi;
+    } else if (bmi >=18.5 && bmi < 25) {
+      result.innerHTML = "Normal weight" + bmi;
+    } else if (bmi < 30) {
+      result.innerHTML = "Overweight"+ bmi;
+    } else {
+      alert('Hasil Error,Ulangi!');
+      result.innerHTML ='';
+    }
+  
+  }
+});
+
